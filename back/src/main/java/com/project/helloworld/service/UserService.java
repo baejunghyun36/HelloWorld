6e7eb3bc32743f02b5ee90f930ab8e8114096087
akdgns12 <akdgns12@naver.com>
2023-04-25 15:43:35 +0900

    [Feat] user API Service

--- back/src/main/java/com/project/helloworld/service/UserService.java
+++ back/src/main/java/com/project/helloworld/service/UserService.java
@@ -6,6 +6,15 @@ import org.springframework.http.ResponseEntity;
 public interface UserService {
 
     ResponseEntity<?> signUp(UserRequestDto.SignUp signUp);
-    ResponseEntity<?> signIn(UserRequestDto.SignIn signIn);
+    ResponseEntity<?> signIn(UserRequestDto.SignIn signIn) throws Exception;
     ResponseEntity<?> reissue(UserRequestDto.Reissue reissue);
+    ResponseEntity<?> getUserInfo(Long userSeq) throws Exception;
+    ResponseEntity<?> modify(UserRequestDto.Modify modify) throws Exception;
+    ResponseEntity<?> changePassword(UserRequestDto.ChangePassword changePassword) throws Exception;
+    ResponseEntity<?> delete(Long userSeq) throws Exception;
+    ResponseEntity<?> certifiedEmail(String email) throws Exception;
+    ResponseEntity<?> confirmEmail(UserRequestDto.EmailCertify emailCertify) throws Exception;
+    ResponseEntity<?> certifiedPhoneNumber(String phoneNumber) throws Exception;
+    ResponseEntity<?> confirmMessage(UserRequestDto.SmsCertify smsCertify) throws Exception;
+    ResponseEntity<?> logout(UserRequestDto.Logout logout) throws Exception;
 }
