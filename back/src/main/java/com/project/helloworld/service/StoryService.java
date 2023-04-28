8ccfbfe4cd6cca8e1d4ac2417409d76b74bb9b3d
whgusehd96 <whgusehd96@gmail.com>
2023-04-28 17:35:24 +0900

    [Feat] async send kafka msg

---
+++ back/src/main/java/com/project/helloworld/service/StoryService.java
@@ -0,0 +1,11 @@
+package com.project.helloworld.service;
+
+import com.project.helloworld.domain.Board;
+import com.project.helloworld.domain.Guestbook;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface StoryService {
+    public void sendStory(Board board);
+}
