7c80ad0310ee7121b010851870c53db4b7dec3b4
akdgns12 <akdgns12@naver.com>
2023-05-01 13:05:48 +0900

    [Feat] domain port cors 추가

--- back/src/main/java/com/project/helloworld/config/SecurityConfig.java
+++ back/src/main/java/com/project/helloworld/config/SecurityConfig.java
@@ -126,7 +126,7 @@ public class SecurityConfig {
         configuration.addAllowedOrigin("http://localhost:3000");
         // 추후 도메인 주소에 맞게 변경필요
         configuration.addAllowedOrigin("http://localhost:8081");
-//        configuration.addAllowedOrigin("http://3.34.141.63:443");
+        configuration.addAllowedOrigin("https://k8a308.p.ssafy.io");
 //        configuration.addAllowedOrigin("https://i8a805.p.ssafy.io");
         configuration.addAllowedHeader("*");
         configuration.addAllowedMethod("*");
