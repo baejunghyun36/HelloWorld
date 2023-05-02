2e4627e48f811266720ab3e497a23b2f3e893e78
whgusehd96 <whgusehd96@gmail.com>
2023-05-02 14:40:48 +0900

    [Feat] story test controller

--- StoryModule/src/main/java/com/project/StoryModule/controller/StoryController.java
+++ StoryModule/src/main/java/com/project/StoryModule/controller/StoryController.java
@@ -1,6 +1,9 @@
 package com.project.StoryModule.controller;
 
+import com.project.StoryModule.security.jwt.JwtAuthenticationFilter;
+import com.project.StoryModule.security.jwt.JwtTokenProvider;
 import com.project.StoryModule.service.StoryService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,21 +12,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/story")
 @Slf4j
 public class StoryController {
     private final StoryService storyService ;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final JwtTokenProvider jwtTokenProvider;
 
+    @ApiOperation(value = "스토리 가져오기")
     @GetMapping("/{storySeq}")
     ResponseEntity<?> getStory(@PathVariable Long storySeq){
         log.info("storySeq: {}", storySeq);
         return storyService.getStory(storySeq);
     }
+    
+    @ApiOperation(value = "모든 스토리 가져오기")
     @GetMapping("/all/{userSeq}")
     ResponseEntity<?> getAllStory(@PathVariable Long userSeq) throws Exception{
         log.info("userSeq: {} ", userSeq);
+//        String token = jwtAuthenticationFilter.parseBearerToken(request);
+//        String email = jwtTokenProvider.getEmail(token);
+
         return storyService.getAllStory(userSeq);
     }
 }
