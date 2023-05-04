a6ef55123640ebe5d37506d2f27ce402e9aeeb4a
akdgns12 <akdgns12@naver.com>
2023-05-04 17:40:13 +0900

    [Chore] 불 불필요한 소스 제거

--- back/src/main/java/com/project/helloworld/interceptor/VisitorInterceptor.java
+++ back/src/main/java/com/project/helloworld/interceptor/VisitorInterceptor.java
@@ -33,33 +33,12 @@ public class VisitorInterceptor implements HandlerInterceptor {
         Long visitorSeq = jwtTokenProvider.getUserSeq(token); // 방문자 seq
         String visitorId = jwtTokenProvider.getUserEmail(token);
 
-        if(ownerSeq == null){
-            System.out.println("--------------ownerSeq 추출 못햇다--------------------------");
-            return true;
-        }
-        if(Long.valueOf(ownerSeq) == visitorSeq){ // 자기 홈페이지 조회시엔 카운팅 안되게끔
+        if(Long.valueOf(ownerSeq) == visitorSeq || ownerSeq == null){ // 자기 홈페이지 조회시엔 카운팅 안되게끔
             return true;
         }
 
         // HyperLogLog을 사용하여 중복 방문자를 제거한 총 방문자 수 및 하루 방문자 수 갱신
         visitorService.addVisitor(ownerSeq, visitorId);
-
-        System.out.println("조회한 사람 seq " + visitorSeq);
-        System.out.println("홈페이지 주인 seq " + ownerSeq);
-
-        System.out.println("----------------------------------------Interceptor-----------------------------------");
-
-        System.out.println("-----------------------------------------today, total---------------------------------");
-        System.out.println("총 방문자 수 " +         visitorService.getTotalVisitors(ownerSeq));
-        System.out.println("오늘 방문자 수 " +         visitorService.getTodayVisitors(ownerSeq));
-
-
-//        redisTemplate.delete(todayVisitorKey);
-//        redisTemplate.delete(totalVisitorKey);
-
-        // Request에 방문자 수 정보를 추가하여 Controller에서 사용
-//        request.setAttribute("todayVisitorCount", todayVisitorCount);
-//        request.setAttribute("totalVisitorCount", totalVisitorCount);
 
         return true;
     }
