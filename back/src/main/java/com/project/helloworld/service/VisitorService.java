4d69adef32771696152ad2cb6055d98db032c613
akdgns12 <akdgns12@naver.com>
2023-05-04 15:04:04 +0900

    [Feat] 하루 방문ã자 총 방문자 service

---
+++ back/src/main/java/com/project/helloworld/service/VisitorService.java
@@ -0,0 +1,9 @@
+package com.project.helloworld.service;
+
+public interface VisitorService {
+    void addVisitor(String email, String visitorId);
+    Long getTotalVisitors(String ownerSeq);
+    Long getTodayVisitors(String ownerSeq);
+    void expireTodayVisitors(String ownerSeq);
+    void refreshTotalVisitors(String ownerSeq);
+}
