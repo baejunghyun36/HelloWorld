881521c12a72d5a8e7f89e501d4fa16de04b9351
akdgns12 <akdgns12@naver.com>
2023-05-08 10:07:35 +0900

    [Feat] 통계 ResponseDto

---
+++ back/src/main/java/com/project/helloworld/dto/StatisticResponseDto.java
@@ -0,0 +1,15 @@
+package com.project.helloworld.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+public class StatisticResponseDto {
+
+    @Getter
+    @Setter
+    @Builder
+    public static class StatisticInfo{
+
+    }
+}
