cad9c7abb0b8369fb88073b5ed0823001ddef564
akdgns12 <akdgns12@naver.com>
2023-05-11 22:45:25 +0900

    [Feat] 불필요한 import 제거

--- back/src/main/java/com/project/helloworld/dto/UserRequestDto.java
+++ back/src/main/java/com/project/helloworld/dto/UserRequestDto.java
@@ -4,7 +4,6 @@ package com.project.helloworld.dto;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.NotEmpty;
 
