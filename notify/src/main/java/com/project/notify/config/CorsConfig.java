e7120842622ffa1723d20c66c7aa7078b5a7f62a
baejunghyun36 <bae1004kin@gmail.com>
2023-05-11 13:41:16 +0900

    [Feat] 알림 서버 cors 로직 추가

--- notify/src/main/java/com/project/notify/config/CorsConfig.java
+++ notify/src/main/java/com/project/notify/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebFluxConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000") // 여기서 '*' 대신 허용하려는 도메인을 명시할 수 있습니다.
+        .allowedOrigins("http://localhost:3000","https://k8a308.p.ssafy.io") // 여기서 '*' 대신 허용하려는 도메인을 명시할 수 있습니다.
         .allowedMethods("*")
         .allowedHeaders("*")
         .allowCredentials(true);
