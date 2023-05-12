02a8049c18f3c855c78b6710f5c1e6d6c8812161
akdgns12 <akdgns12@naver.com>
2023-05-12 10:39:27 +0900

    [Feat] ì기본 유저정보 조회에 일촌여부 추가

--- back/src/main/java/com/project/helloworld/service/UserServiceImpl.java
+++ back/src/main/java/com/project/helloworld/service/UserServiceImpl.java
@@ -187,6 +187,12 @@ public class UserServiceImpl implements UserService{
                 .authProvider(user.getAuthProvider())
                 .build();
 
+        String userEmail = SecurityUtil.getCurrentUserEmail();
+        user = userRepository.findByEmail(userEmail).orElseThrow(()-> new Exception("해당하는 유저가 없습니다." + userSeq));
+        Long visitorSeq = user.getUserSeq();
+        Long masterSeq = userSeq;
+        userInfo.setIsFamily(familyService.getFamilyByUser(masterSeq, visitorSeq));
+
         return response.success(userInfo, "유저 정보가 조회되었습니다.", HttpStatus.OK);
     }
 
@@ -251,8 +257,8 @@ public class UserServiceImpl implements UserService{
         String userEmail = SecurityUtil.getCurrentUserEmail();
         user = userRepository.findByEmail(userEmail).orElseThrow(()-> new Exception("해당하는 유저가 없습니다." + userSeq));
         Long visitorSeq = user.getUserSeq();
-        Long masterSEq = userSeq;
-        userMainInfo.setIsFamily(familyService.getFamilyByUser(masterSEq, visitorSeq));
+        Long masterSeq = userSeq;
+        userMainInfo.setIsFamily(familyService.getFamilyByUser(masterSeq, visitorSeq));
 
         return response.success(userMainInfo, "유저 메인페이지 정보 조회가 성공했습니다.", HttpStatus.OK);
     }
