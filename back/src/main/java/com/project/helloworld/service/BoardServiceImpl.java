dad71785ad4790ecd77960394bfbaab90207b661
baejunghyun36 <bae1004kin@gmail.com>
2023-05-15 13:09:29 +0900

    [Feat] 소나큐브 적용

--- back/src/main/java/com/project/helloworld/service/BoardServiceImpl.java
+++ back/src/main/java/com/project/helloworld/service/BoardServiceImpl.java
@@ -14,6 +14,7 @@ import com.project.helloworld.repository.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.index.query.BoolQueryBuilder;
@@ -38,6 +39,7 @@ import java.util.stream.Collectors;
 
 @Service
 @Slf4j
+@Builder
 @RequiredArgsConstructor
 public class BoardServiceImpl implements BoardService{
 
